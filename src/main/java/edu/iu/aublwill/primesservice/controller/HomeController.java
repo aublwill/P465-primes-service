da7bc0e8e770c9c2dc51728172a3744a06c6a45e
aublwill <aublwill@iu.edu>
2024-02-28 14:55:36 -0500

    commit

--- src/main/java/edu/iu/aublwill/primesservice/controller/HomeController.java
+++ src/main/java/edu/iu/aublwill/primesservice/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String greetings(){
-        return " to the primes service!";
+        return "Welcome to the primes service!";
     }
 }
